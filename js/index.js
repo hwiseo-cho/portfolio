3d192df4d6c29b7db6ed58c7f9db68d0787155c7
hwiseo-cho <57250330+hwiseo-cho@users.noreply.github.com>
2020-11-25 01:48:33 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,7 +1,3 @@
-var script = document.createElement('script');
-script.src = 'https://code.jquery.com/jquery-3.5.1.min.js';
-script.type = 'text/javascript';
-document.getElementsByTagName('head')[0].appendChild(script);
 
 $(function(){
     $("#Profile").on("click",function(){
